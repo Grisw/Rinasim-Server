e34e854c14271eafa1c8a45d41290d1550c7531e
刘旭涛 <514335188@qq.com>
2015-03-14 15:49:56 +0800

    First Blood

---
+++ src/org/rinasim/main/Main.java
@@ -0,0 +1,37 @@
+package org.rinasim.main;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * 启动程序的入口
+ * @since v1.0
+ * @author 刘旭涛
+ * @date 2015年3月14日 下午1:39:56
+ */
+public class Main {
+
+	/**
+	 * 主处理
+	 * @author 刘旭涛
+	 * @date 2015年3月14日 下午1:42:41
+	 * @since v1.0
+	 */
+	public static void main(String[] args) {
+		
+		//设定LookAndFeel
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
