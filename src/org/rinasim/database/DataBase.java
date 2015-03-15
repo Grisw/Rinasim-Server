d96bdb0dbf6093af372387bc9cbb41768ab35dff
刘旭涛 <514335188@qq.com>
2015-03-15 09:57:50 +0800

    UI设定及数据库配置

---
+++ src/org/rinasim/database/DataBase.java
@@ -0,0 +1,40 @@
+package org.rinasim.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+/**
+ * 连接数据库
+ * @author MissingNo.
+ * @date 2014年12月15日 下午9:06:49
+ * @version v3.0
+ */
+public class DataBase {	
+	
+	private static String user="sa";
+	private static String password="MissingNo.";
+	private static String className="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static String url="jdbc:sqlserver://localhost:1433;DatabaseName=Rinasim";
+	
+	/**
+	 * 获取连接
+	 * @return 与数据库的连接对象
+	 */
+	public static Connection getConnection(){
+		try {
+			Class.forName(className);
+			return DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "连接数据库失败！\n"+e.getMessage(), "错误", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "连接数据库失败！\n"+e.getMessage(), "错误", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
